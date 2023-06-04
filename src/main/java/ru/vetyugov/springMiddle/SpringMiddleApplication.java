f64de3bcc60e60e59d23f6d5cf25691027407986
VetyugovN <NSVetyugov@rosatom.ru>
2023-06-04 20:10:10 +0300

    Initial commit

---
+++ src/main/java/ru/vetyugov/springMiddle/SpringMiddleApplication.java
@@ -0,0 +1,13 @@
+package ru.vetyugov.springMiddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMiddleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMiddleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vetyugov/springMiddle/SpringMiddleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vetyugov.springMiddle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMiddleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
